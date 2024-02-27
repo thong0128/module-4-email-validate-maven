af742fd39acbe44e9af308c8a96460f31ef54ad9
Thống Nguyễn <thong0128@gmail.com>
2024-02-27 14:23:32 +0700

    Done

--- src/main/java/com/codegym/emailvalidatemaven/HomeController.java
+++ src/main/java/com/codegym/emailvalidatemaven/HomeController.java
@@ -1,2 +1,32 @@
-package com.codegym.emailvalidatemaven;public class HomeController {
+package com.codegym.emailvalidatemaven;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.regex.Pattern;
+
+@Controller
+public class HomeController {
+    private static final String EMAIL_REGEX = "^[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)$";
+    private static Pattern pattern;
+    public HomeController() {
+        pattern = Pattern.compile(EMAIL_REGEX);
+    }
+    @GetMapping(value = "/")
+    public String home(){
+        return "home";
+    }
+    @PostMapping(value = "/validate")
+    public String user(@RequestParam("email") String email, ModelMap model){
+        boolean isValid = pattern.matcher(email).matches();
+        if(!isValid){
+            model.addAttribute("message", "Your email is invalid");
+            return "home";
+        }
+        model.addAttribute("email", "Your email is valid");
+        return "success";
+    }
 }
--- src/main/webapp/WEB-INF/views/success.jsp
+++ src/main/webapp/WEB-INF/views/success.jsp
@@ -1,16 +1,9 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: thong
-  Date: 27/02/2024
-  Time: 2:15 CH
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
-    <title>$Title$</title>
-  </head>
-  <body>
-  $END$
-  </body>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h3>Hi ${email}</h3>
+</body>
 </html>
